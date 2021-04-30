c7758f015f8e7f2daa4048ade6dcd9f88b6e5937
dnatuna <dnatuna123@gmail.com>
2021-05-01 05:20:37 +0900

    feat: mypage API 기능 구현 완료

--- src/controllers/ProfileController.js
+++ src/controllers/ProfileController.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import * as AuthHelper from '../middlewares/AuthHelper';
 import * as ProfileService from '../services/ProfileService';
+
 const router = express.Router();
 
-router.get('/', ProfileService.findById);
+router.get('/', AuthHelper.isLoggedIn, ProfileService.findById);
+router.patch('/woori', AuthHelper.isLoggedIn, ProfileService.updateWooriAccount);
+router.patch('/coin', AuthHelper.isLoggedIn, ProfileService.updateCoinAccount);
 
 export default router;
